33ffc75c5e676fe171e49d3e1dda7fb8625dbc07
Alexey Lutin <lutin@smedia.ru>
2021-08-12 15:34:38 +0300

    added subnet for docker-compose

--- app/manage.py
+++ app/manage.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if os.getenv("FLASK_ENV") == "development":
         app.run(
             debug=True,
-            host='0.0.0.0',
+            host='10.10.0.10',
             port=3031
         )
     else:
